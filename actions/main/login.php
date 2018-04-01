49e449060f8db81b648383fbe07d88b0bd0dfa34
Trillnonymous <trillnonymous@gmail.com>
2018-04-01 18:43:18 -0400

    Create login.php

---
+++ actions/main/login.php
@@ -0,0 +1,7 @@
+<?php
+	$username = isset($_GET['username']) ? $_GET['username'] : '';
+	$password = isset($_GET['password']) ? $_GET['password'] : '';
+	$data = array();
+	
+	if ($username !== ''){}
+?>
