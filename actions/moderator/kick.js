971ae4fdbebfdedd7e04c6cf26228ca41a53b771
Trillnonymous <trillnonymous@gmail.com>
2018-04-01 18:46:02 -0400

    Create kick.js

---
+++ actions/moderator/kick.js
@@ -0,0 +1,7 @@
+<?php
+	$username = isset($_GET['username']) ? $_GET['username'] : '';
+	$target = isset($_GET['target']) ? $_GET['target'] : '';
+	$data = array();
+	
+	if ($username !== '' && $target !== ''){}
+?>
