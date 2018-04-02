cde308a399b79c1eb532f158cdb0a40271e103fd
Trillnonymous <trillnonymous@gmail.com>
2018-04-01 22:22:25 -0400

    Update kick.js

--- actions/moderator/kick.js
+++ actions/moderator/kick.js
@@ -1,7 +1,26 @@
 <?php
 	$username = isset($_GET['username']) ? $_GET['username'] : '';
 	$target = isset($_GET['target']) ? $_GET['target'] : '';
+	$roomid = isset($_GET['roomid']) ? $_GET['roomid'] : '';
 	$data = array();
 	
-	if ($username !== '' && $target !== ''){}
+	if ($username !== '' && $target !== '' && $roomid !== ''){
+		$userstable = 'chat_' . $roomid . '_users';
+		$query = mysql_query("SELECT * FROM `$userstable` WHERE username='$target'");
+		$rows = mysql_num_rows($query);
+		if ($rows === 0){
+			$data['error'] = 'Unable to kick because the user is not present in the chat';
+			$data['error_code'] = 101;
+		} else {
+			$roomtable = 'chat_' . $roomid . '_users';
+			$sql = mysql_query("DELETE FROM `$roomtable` WHERE username='$target'");
+			$roomlogtable = 'chat_' . $roomid . '_logs';
+			$roomlogtext = $target . ' has been kicked by ' . $username '.';
+			$sql2 = mysql_query("INSERT INTO `$roomlogtable` (`type`, `message`) VALUES ('kick', '$roomlogtext')");
+			$data['user'] = $username;
+			$data['target'] = $target;
+			$data['message'] = $roomlogtext;
+		}
+	}
+	echo json_encode($data);
 ?>
